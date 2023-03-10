df2522f3e35218d5c0d4e16cc7e08760b9d813e0
Christian Verges <114942077+ChrisVerg@users.noreply.github.com>
2023-03-10 13:31:34 -0600

    Update VAERS_Project.R

--- VAERS_Project.R
+++ VAERS_Project.R
@@ -1,13 +1,5 @@
-# The purpose of this project if to learn how to and improve my coding abilities
-# I will follow COVID injuries with data straight from Vaccine Adverse Event 
-# Reporting system (VAERS).
+# Evaluating what percent of all vaccine injury reports were due to COVID-19 vaccine injuries 
 
-# I intend to develop and examine the data to compare the injury rate compared to
-# that of other vaccines. This will require other data of total estimated vaccines
-# delivered. 
-
-# Also, I will try to break down the data into severe reactions and then 
-# further what the demographic of severe reactions were by age and risk factors. 
 
 install.packages("ggplot2")
 install.packages("dplyr")
@@ -96,46 +88,3 @@ covid_percent <- (only_covid_count / all_reported_count) * 100
 covid_percent
 
 
-# Next Compare the same data for other years to create a trend line. 
-
-# Ignore all of this, I am just looking at old code that worked for other stuff
-# Ok, now compare the years that the compare_5 are higher than the compare_all
-
-compared1 <- compare_5$med.life.5 > compare_all$med.life
-view(compared1)
-
-# which returns which were true by index
-RowsTrue <- which(compared1 == TRUE)
-RowsVec <- RowsTrue
-RowsVec
-
-# slice returns the rows by index to retreieve the data by row number
-years_with_higher_gdp <- compare_5 %>% slice(9,11,12)
-years_with_higher_gdp
-
-only_true <- compare_5 %>% slice(RowsVec)
-view(only_true)
-
-
-
-dplyr::filter(VAERS_ID_VAX, !grepl("COVID19",TrackingPixel))
-
-dplyr::filter(df, !grepl("RTB",TrackingPixel))
-
-COVID_vector <- VAERS_ID_VAX$VAX_TYPE
-view(COVID_vector)
-
-covid_TF <- COVID_vector == "COVID19"
-view(covid_TF)
-
-three_by_df <- data.frame(covid_TF, VAERS_ID_VAX)
-view(three_by_df)
-
-only_covid <- COVID_vector == "COVID19"
-view(COVID_vector)
-
-
-
-tail(only_covid)
-length(only_covid)
-count(VAERS_ID_VAX)
